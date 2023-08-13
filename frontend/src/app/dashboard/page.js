aeb556bbb5b89ac5bdb9ad7ca5216f5e6298ff3d
Lucas Santos Rodrigues <70177902+LuSrodri@users.noreply.github.com>
2023-08-13 03:51:55 -0300

    estilizando dashboard

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,14 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './dashboard.module.css'
 import Link from 'next/link'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCloudShowersHeavy, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default function Dashboard() {
   return (
     <main className={styles.main}>
+      <div className={styles.topbar}>
+        <h3><Link href={'/sair'}><FontAwesomeIcon icon={faRightFromBracket} /></Link></h3>
+        <h3 className={styles.user}><FontAwesomeIcon icon={faUser} /> Lucas Santos Rodrigues</h3>
+      </div>
 
-      <div className={styles.header}>
-        <div>
+      <div className={[styles.firstcity, styles.raining].join(' ')}>
+        <div style={{ minWidth: "60%" }}>
+          <h2><FontAwesomeIcon icon={faCloudShowersHeavy} /></h2>
           <h3>Chuva Forte</h3>
           <h1>19°</h1>
           <h2>São Paulo</h2>
@@ -19,12 +24,12 @@ export default function Dashboard() {
         </div>
       </div>
 
-      <div className={styles.info}>
-        <div className={styles.topbar}>
-          <h1>Temperaturas</h1>
-          <button><FontAwesomeIcon icon={faPlus} /> Adicionar cidade</button>
+      <div className={styles.allcities}>
+        <div className={styles.addcity}>
+          <h2>Temperaturas</h2>
+          <input placeholder={"+ Adicionar cidade"}></input>
         </div>
-        <div>
+        <div className={styles.cities}>
           // cidades
         </div>
       </div>
